053d04ba93d42f108f070f428fa9de17bc039206
Templin <ajtempl@ilstu.edu>
2020-10-02 14:51:37 -0500

    created hello world

---
+++ src/edu/ilstu/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Created on: Aug 20, 2020
+ * 
+ * ULID: ajtempl
+ * Class: IT 168
+ */
+package edu.ilstu;
+
+/**
+ * <Traditional first program that displays "Hello, world!">
+ *
+ * @author Andrew Templin
+ *
+ */
+public class HelloWorld
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, world!");
+
+	}
+
+}
